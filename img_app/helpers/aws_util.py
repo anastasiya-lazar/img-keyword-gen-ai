4aba15659f6c09f6d586c02b9d8f993d687d6c69
anastasiya-lazar <anastasiya.lazar@dubizzle.com>
2024-08-30 15:20:28 +0400

    ⚡️ Search by folder

--- img_app/helpers/aws_util.py
+++ img_app/helpers/aws_util.py
@@ -1,35 +1,50 @@
-import logging
+import boto3
 import os
 
-import boto3
 
 def list_s3_objects(bucket_name, prefix='', region_name='eu-west-1'):
     """
     List all objects in a public S3 bucket with a given prefix.
-    Returns a list of dictionaries containing object keys and URLs.
+    Returns a list of dictionaries containing object keys and URLs. Use AWS credentials if provided.
     """
-    # Initialize the S3 client with specified credentials
-    s3_client = boto3.client('s3',
-                             region_name=region_name,
-                             aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-                             aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'))
+    session = boto3.Session(
+        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+        aws_session_token=os.getenv('AWS_SESSION_TOKEN'),
+        region_name=region_name
+    )
+    s3_client = session.client('s3')
 
-    logging.info(f"Listing objects in bucket {bucket_name} with prefix '{prefix}'")
+    print(f"Listing objects in bucket {bucket_name} with prefix {prefix}")
 
-    try:
-        response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+    continuation_token = None
+    objects = []
 
-        objects = []
+    while True:
+        list_kwargs = {
+            "Bucket": bucket_name,
+            "Prefix": prefix
+        }
+        if continuation_token:
+            list_kwargs['ContinuationToken'] = continuation_token
+
+        response = s3_client.list_objects_v2(**list_kwargs)
+
         if 'Contents' in response:
             for obj in response['Contents']:
-                # Check for specific image file extensions
-                if obj['Key'].endswith(('.jpg', '.jpeg', '.png', '.svg')):
+                if not obj['Key'].endswith('/'):
+                    # Generate URL for each object
                     object_url = f"https://{bucket_name}.s3.{region_name}.amazonaws.com/{obj['Key']}"
+                    # Add object key and URL to the list
                     objects.append({'key': obj['Key'], 'url': object_url})
 
-        logging.info(f"Found {len(objects)} objects in bucket {bucket_name} with prefix '{prefix}'")
-        return objects
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-        return []
+        # Check if there are more pages; if not, exit the loop
+        if response.get('IsTruncated'):
+            continuation_token = response.get('NextContinuationToken')
+        else:
+            break
 
+    print(f"Found {len(objects)} objects in bucket {bucket_name} with prefix {prefix}")
+    print(objects)
+
+    return objects
